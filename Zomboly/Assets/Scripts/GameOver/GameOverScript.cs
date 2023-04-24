fcd6ca91eb6062521207fa4a08859e3018a6d449
rlathdud1972 <rlathdud1972@gmail.com>
2023-04-24 15:17:22 +1200

    GameOver and Health variable
    Trying to access the health variable so that I can trigger the game over scene when the health variable reaches 0 but I think the variable is not set to public.

--- Zomboly/Assets/Scripts/GameOver/GameOverScript.cs
+++ Zomboly/Assets/Scripts/GameOver/GameOverScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class GameOverScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public UnitHealth script;
+
+    public void Start()
     {
-        
+      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
