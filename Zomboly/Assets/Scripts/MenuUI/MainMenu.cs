e4253d42d9ca5b476030ea4731a683cc956a3ff7
rlathdud1972 <rlathdud1972@gmail.com>
2023-05-04 20:46:01 +1200

    Code explained
    Comments added to the methods/functions

--- Zomboly/Assets/Scripts/MenuUI/MainMenu.cs
+++ Zomboly/Assets/Scripts/MenuUI/MainMenu.cs
@@ -5,11 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    //Next scene once player clicks the Play button.
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+
+    //Exit game once the player clicks the Quit button.
     public void Quit()
     {
         Application.Quit();
