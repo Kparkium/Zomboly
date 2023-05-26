ee5209dce6e41fb5e7cc155dfb8545cb77be8bd3
Brad <Bradkivell@gmail.com>
2023-05-26 17:13:14 +1200

    SetupTesting

---
+++ Zomboly/Assets/PlayTestAssembly/Brad/UnitHealth.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitHealth : MonoBehaviour 
+{
+    //Fields (these are default private)
+    public int _currentHealth;
+    public int _currentMaxHealth;
+
+    //Properties
+    //You use these to change and update the fields instead of directly changing the fields.
+    public int Health
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        set
+        {
+            _currentHealth = value;
+        }
+    }
+
+    public int MaxHealth
+    {
+        get
+        {
+            return _currentMaxHealth;
+        }
+        set
+        {
+            _currentMaxHealth = value;
+        }
+    }
+
+    //Constructor
+    public void init(int health, int maxHealth)
+    {
+        _currentHealth = health;
+        _currentMaxHealth = maxHealth;
+    }
+
+    //Methods
+
+    //use this method when an unit is damaged.
+    public void DamageUnit(int dmgAmount)
+    {
+
+        if (_currentHealth > 0)
+        {
+            _currentHealth -= dmgAmount;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void HealUnit(int healAmount)
+    {
+        if (_currentHealth < _currentMaxHealth)
+        {
+            _currentHealth += healAmount;
+        }
+
+        if (_currentHealth > _currentMaxHealth)
+        {
+            _currentHealth = _currentMaxHealth;
+        }
+    }
+}
